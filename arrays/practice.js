aa16535678c51b77a9b238f9e97fc2442d5adfa9
Nathaniel Shirts <nshirts7@gmail.com>
2021-01-25 21:05:48 -0700

    finished part 1 of the array practice

--- arrays/practice.js
+++ arrays/practice.js
@@ -19,6 +19,10 @@ var arr = [10,20,30];
 
 //Code Here
 
+function first(arr){
+  return arr[0];
+}
+
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +37,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr) {
+  return arr[(arr.length-1)]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +54,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    alert(arr[i])
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +73,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(arr) {
+  for (i = arr.length-1; i >= 0; i--) {
+    alert(arr[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +92,9 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder(nums) {
+  return nums.filter(even => even%2 == 0);
+}
 
 
 
